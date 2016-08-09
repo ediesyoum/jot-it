d38f3b27483116b0d3a74f3567993241cd32bed7
Eden Syoum <eden@temple.edu>
2016-08-09 07:47:19 -0400

    troubleshot authentication issues and logins are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
 
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.username || self.email
-  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :notes, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:email]
+
+
+
+    has_many :notes, dependent: :destroy
+
+    # Virtual attribute for authenticating by either username or email
+    # This is in addition to a real persisted field like 'username'
+    attr_accessor :login
 end
